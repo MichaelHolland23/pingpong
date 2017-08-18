4be2958caefeff9e0c21a63487f9dcb55e2a06a2
Mike Holland <michaellholland@hotmail.com>
2017-08-18 13:51:44 -0700

    begin for loop, add initial if elses

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("form#pingpong").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("#number").val());
+
+    for (var index =1; index <= input; index +1) {
+      if (index % 3 === 0) {
+      }  else if (index % 5 === 0) {
+      }  else if (index % 15 === 0) {
+        return pingList
+
+        }
+      }
+    }
