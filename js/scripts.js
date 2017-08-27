53404b162a809749ec0dbd8f8893a5d6f4528208
Mike Holland <michaellholland@hotmail.com>
2017-08-26 23:54:11 -0700

    add styling to index page, add images, rearrange business and user logic on scripts page

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
-$(document).ready(function() {
-  $("form#pingpong").submit(function(event) {
-    event.preventDefault();
-    var input = parseInt($("#number").val());
 
-    for (var index =1; index <= input; index +1) {
-      if (index % 3 === 0) {
-      }  else if (index % 5 === 0) {
-      }  else if (index % 15 === 0) {
-        return pingList
 
-      listArray.forEach(function(listItem){
-      $('ul#final').append("<li>" + listItem + "</li>");
+for (var index =1; index <= input; index +1) {
+  if (index % 3 === 0) {
+  }  else if (index % 5 === 0) {
+  }  else if (index % 15 === 0) {
+    return pingList
 
-      };
-    }
-  }
+  listArray.forEach(function(listItem){
+  $('ul#final').append("<li>" + listItem + "</li>");
+      });
+    };
+  };
+
+  $(document).ready(function() {
+    $("#pingpong").submit(function(event) {
+      event.preventDefault();
+      var input = parseInt($("#number").val());
+        alert(input);
+    });
+  });
