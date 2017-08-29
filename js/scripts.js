ff37d934c824098f0c67b6868a78204d6c5f81be
Mike Holland <michaellholland@hotmail.com>
2017-08-28 19:48:07 -0700

    troubleshoot list items and if elses

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,40 @@
 function begin(input) {
+
   var sum = []
 
-  for (var index = 1; <= input; index +=1) {
+  for (var index = 1; index <= input; index += 1) {
     sum.push(index);
     if ((index % 3) === 0) {
       sum.pop(index)
       sum.push("ping");
-      else if ((index % 5) === 0) {
+    } else if ((index % 5) === 0) {
       sum.pop(index)
       sum.push("pong");
-      else ((index % 15) === 0) {
+    } else if ((index % 15) === 0) {
       sum.pop(index)
       sum.push("PING-PONG!");
-    return sum
     }
   }
+  return sum
 }
 
-  $(document).ready(function() {
-    $("#pingpong").submit(function(event) {
-      event.preventDefault();
+$(document).ready(function() {
+  $("form#pingpong").submit(function(event) {
+    event.preventDefault();
 
-      var input = parseInt($("#number").val());
-        alert(input);
+    var input = parseInt($("input#number").val());
+    var result = begin(input);
+
+    var listArray = result.map(function(item)) {
+        return ("<li>" + item + "</li>")
+    })
+
+    if (isNaN(input)) {
+      $("#notNum").show();
+    }
+
+    $("ul#final").empty();
+    $("ul#final").prepend(list);
+
     });
   });
