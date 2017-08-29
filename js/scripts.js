f2ff07f14c96ceaac80fd046121868d1aac98a36
Mike Holland <michaellholland@hotmail.com>
2017-08-28 18:32:48 -0700

    fix if elses for input, check values with alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
+function begin(input) {
+  var pong = []
+
+  for (var index = 1; <= input; index +=1) {
+    pong.push(index);
+    if (index % 3 = 0) {
+      pong.pop(index)
+      pong.push("ping");
+      else (index % 5 = 0);
+      pong.pop(index)
+      pong.push("pong");
+      else (index % 15 = 0);
+      pong.pop(index)
+      pong.push("PING-PONG!");
+    return pong
+    }
+  }
+}
 
 
-for (var index =1; index <= input; index +1) {
-  if (index % 3 === 0) {
-  }  else if (index % 5 === 0) {
-  }  else if (index % 15 === 0) {
-    return pingList
 
-  listArray.forEach(function(listItem){
-  $('ul#final').append("<li>" + listItem + "</li>");
-      });
-    };
-  };
+
+
+
 
   $(document).ready(function() {
     $("#pingpong").submit(function(event) {
       event.preventDefault();
+
       var input = parseInt($("#number").val());
         alert(input);
     });
